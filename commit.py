e5d0729ce181b56cbb4d08a5e01cf4e8e60fd049
Alejandro Barrena Millán <abarrenaq@alumnos.unex.es>
2024-10-31 19:10:00 +0100

    SCRUM-72 Integración del eliminar

--- commit.py
+++ commit.py
@@ -40,7 +40,7 @@ def change_commit_author(authors):
     except subprocess.CalledProcessError as e:
         print(f"\nError al modificar el autor: {str(e)}")
 
-
+# Obtiene la rama actual
 def get_current_branch():
     return subprocess.check_output(['git', 'rev-parse', '--abbrev-ref', 'HEAD']).decode('utf-8').strip()
 
